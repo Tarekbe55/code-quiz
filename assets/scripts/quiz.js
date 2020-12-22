b78bf45e5fd9a64153930fdc1271e1dd31852abd
Tarek Behairy <tbtarek@gmail.com>
2020-12-22 14:55:44 -0500

    added complete page in html file and list of questions in JS

---
+++ assets/scripts/quiz.js
@@ -0,0 +1,33 @@
+var questionList = [
+  {
+    question: "Which of these variable types can be stored in arrays?",
+    answers: ["numbers", "strings", "booleans", "all of the above"],
+    correctAnswer: 4
+  },
+  {
+    question: "What character is used to bracket the contents of an array?",
+    answers: ["quotation mark", "square bracket", "curly bracket", "angle bracket"],
+    correctAnswer: 2
+  },
+  {
+    question: "Which of these functions displays a message to the user with only an 'OK' button?",
+    answers: ["prompt()", "confirm()", "check()", "alert()"],
+    correctAnswer: 4
+  },
+  {
+    question: "What function do you call to print something to the console?",
+    answers: ["console.log()", "console()", "log()", "print()"],
+    correctAnswer: 1
+  },
+  {
+    question: "Which of the following will check if the variable 'x' is equal in both value and type to the variable 'y'?",
+    answers: ["x = y", "x == y", "x === y", "x != y"],
+    correctAnswer: 3
+  },
+  {
+    question: "Which of these keywords is used to create a loop with an initialization, a condition, and an iterator?",
+    answers: ["while", "do", "for", "loop"],
+    correctAnswer: 3
+  },
+];
+
